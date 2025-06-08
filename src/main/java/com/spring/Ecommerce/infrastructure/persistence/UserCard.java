a6ae782968189edd831e75a8350eeeabf68f838e
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-06-08 04:10:25 -0400

    feat(infra): created and updated Entities

---
+++ src/main/java/com/spring/Ecommerce/infrastructure/persistence/UserCard.java
@@ -0,0 +1,28 @@
+package com.spring.Ecommerce.infrastructure.persistence;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user_card")
+public class UserCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_card;
+    private int cardNumber;
+    private int password;
+    private int securityCode;
+    private String cardHolderName;
+    private String expirationDate;
+
+    @OneToOne
+    @JoinColumn(name = "id_user_card", referencedColumnName = "id_user")
+    private UsersEntity user;
+}
