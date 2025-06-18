ad94ced4301f2f3e71ab6d6fce85fafea51a98c8
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-06-18 18:36:23 -0400

    feat(infra): user repository and controller

--- src/main/java/com/spring/Ecommerce/infrastructure/Repositories/UserEntityRepository.java
+++ src/main/java/com/spring/Ecommerce/infrastructure/Repositories/UserEntityRepository.java
@@ -1,7 +1,9 @@
 package com.spring.Ecommerce.infrastructure.Repositories;
 
+import com.spring.Ecommerce.core.entities.User;
 import com.spring.Ecommerce.infrastructure.persistence.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserEntityRepository extends JpaRepository<Users, Long> {
+    User findByDocument(String document);
 }
--- src/main/java/com/spring/Ecommerce/infrastructure/controllers/UsersController.java
+++ src/main/java/com/spring/Ecommerce/infrastructure/controllers/UsersController.java
@@ -4,6 +4,7 @@ import com.spring.Ecommerce.core.entities.User;
 import com.spring.Ecommerce.infrastructure.dto.UsersEntityDTO;
 import com.spring.Ecommerce.infrastructure.mapper.UserEntityMapper;
 import com.spring.Ecommerce.infrastructure.persistence.Users;
+import com.spring.Ecommerce.infrastructure.services.FindUserByDocumentImp;
 import com.spring.Ecommerce.infrastructure.services.UserCreateImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,19 +17,26 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
 
     private final UserCreateImp userCreateImp;
+    private final FindUserByDocumentImp findUserByDocumentImp;
     private final UserEntityMapper userEntityMapper;
 
     @Autowired
-    public UsersController(UserCreateImp userCreateImp, UserEntityMapper userEntityMapper) {
+    public UsersController(UserCreateImp userCreateImp, FindUserByDocumentImp findUserByDocumentImp, UserEntityMapper userEntityMapper) {
         this.userCreateImp = userCreateImp;
+        this.findUserByDocumentImp = findUserByDocumentImp;
         this.userEntityMapper = userEntityMapper;
     }
 
     @PostMapping("/create")
     public ResponseEntity<User> createUser(@RequestBody UsersEntityDTO userEntityDTO) {
         User recieveUser = this.userEntityMapper.usersEntity(userEntityDTO);
-        User savedUser = this.userCreateImp.execute(recieveUser;
+        User savedUser = this.userCreateImp.execute(recieveUser);
         return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/find/{document}")
+    public ResponseEntity<User> findUserByDocument(@PathVariable String document) {
+        return new ResponseEntity<User>(this.findUserByDocumentImp.execute(document), HttpStatus.FOUND);
     }
 
 }
