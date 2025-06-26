c6996055c043ce941e297d48c55859f8dc22ee31
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-06-26 00:13:56 -0400

    feat(infra): Config to new endpoints and personalized querys on Jpa

--- src/main/java/com/spring/Ecommerce/infrastructure/controllers/Users/UserDeleteController.java
+++ src/main/java/com/spring/Ecommerce/infrastructure/controllers/Users/UserDeleteController.java
@@ -1,7 +1,7 @@
 package com.spring.Ecommerce.infrastructure.controllers.Users;
 
-import com.spring.Ecommerce.infrastructure.persistence.Users;
-import com.spring.Ecommerce.infrastructure.services.UserDeleteImp;
+import com.spring.Ecommerce.infrastructure.Repositories.UserEntityRepository;
+import com.spring.Ecommerce.infrastructure.services.User.UserDeleteImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ap1/v1/users")
 public class UserDeleteController {
+
     private final UserDeleteImp deleteImp;
+    private final UserEntityRepository entityRepository;
 
     @Autowired
-    public UserDeleteController(UserDeleteImp deleteImp) {
+    public UserDeleteController(UserDeleteImp deleteImp, UserEntityRepository entityRepository) {
         this.deleteImp = deleteImp;
+        this.entityRepository = entityRepository;
     }
 
     @DeleteMapping("/deleteUser/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        this.deleteImp.execute(id);
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        if (this.entityRepository.existsById(id)){
+            this.deleteImp.execute(id);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
