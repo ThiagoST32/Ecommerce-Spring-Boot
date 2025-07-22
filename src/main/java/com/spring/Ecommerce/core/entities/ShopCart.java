faab00aaa86168983e6e1a6afc122cc0370a1eeb
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-07-21 23:57:57 -0400

    feat(core): Shopping cart class

--- src/main/java/com/spring/Ecommerce/core/entities/ShopCart.java
+++ src/main/java/com/spring/Ecommerce/core/entities/ShopCart.java
@@ -3,8 +3,54 @@ package com.spring.Ecommerce.core.entities;
 import java.math.BigDecimal;
 import java.util.List;
 
-public record ShopCart(
-        Long id_shopCart,
-        List<Products> products,
-        BigDecimal totalPrice,
-        Long id_user ) {}
+public class ShopCart {
+    private Long id_shopCart;
+    private List<Products> productsList;
+    private BigDecimal totalPrice;
+    private Long id_user;
+
+    public ShopCart(Long id_shopCart, List<Products> products, BigDecimal totalPrice, Long id_user) {
+        this.id_shopCart = id_shopCart;
+        this.productsList = products;
+        this.totalPrice = totalPrice;
+        this.id_user = id_user;
+    }
+
+    public ShopCart(List<Products> products, BigDecimal totalPrice, Long id_user) {
+        this.productsList = products;
+        this.totalPrice = totalPrice;
+        this.id_user = id_user;
+    }
+
+    public Long getId_shopCart() {
+        return id_shopCart;
+    }
+
+    public void setId_shopCart(Long id_shopCart) {
+        this.id_shopCart = id_shopCart;
+    }
+
+    public List<Products> getProductsList() {
+        return productsList;
+    }
+
+    public void setProductsList(List<Products> productsList) {
+        this.productsList = productsList;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(Long id_user) {
+        this.id_user = id_user;
+    }
+}
--- src/main/java/com/spring/Ecommerce/core/gateway/ShopCartGateway/AddProductOnShopCartGateway.java
+++ src/main/java/com/spring/Ecommerce/core/gateway/ShopCartGateway/AddProductOnShopCartGateway.java
@@ -3,6 +3,8 @@ package com.spring.Ecommerce.core.gateway.ShopCartGateway;
 import com.spring.Ecommerce.core.entities.Products;
 import com.spring.Ecommerce.core.entities.ShopCart;
 
+import java.util.List;
+
 public interface AddProductOnShopCartGateway {
     ShopCart execute(Products products);
 }
